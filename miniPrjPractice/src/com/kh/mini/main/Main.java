ea7c28d7f9bdf572daff6beff04b741b5733bdc4
DESKTOP-2G50SHI\user1 <corey5361@nate.com>
2023-10-23 17:59:28 +0900

    23.10.23 미니플젝 man 파일 추가못해서 여기다 백업함 <- 관리자 기능 구현 완성함 (복사 해야 함) 작성자 : 양덕기

--- miniPrjPractice/src/com/kh/mini/main/Main.java
+++ miniPrjPractice/src/com/kh/mini/main/Main.java
@@ -8,16 +8,24 @@ import com.kh.mini.member.vo.MemberVo;
 
 public class Main {
 
-	public static Scanner SC = new Scanner(System.in);
+	public static final Scanner SC = new Scanner(System.in);
 	public static MemberVo loginMember;
-
+//	public static ManagerVo loginManager;
+	
 	public static void main(String[] args) {
+
 		MemberController memberController = new MemberController();
+//		ManagerController managerController = new ManagerController();
 		
-		
-		System.out.println("미니프로젝트 연습_01");
 		while(true) {
-			memberController.memberMenu();
+			System.out.println("1: 유저 2: 관리자");
+			String num = Main.SC.nextLine();
+			
+			switch(num) {
+			case "1" : while(true) { memberController.memberMenu();} 
+//			case "2" : while(true) { managerController.managerMenu();} 
+			default : System.out.println("잘못 입력하였습니다.");
+			}
 		}
 	}
 
